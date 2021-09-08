31e0b96a042b757205ae20d502f4c8a330667fb9
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 13:08:10 +0200

    'Update'

--- ImageRecorder/scripts/ImageRecorder.lua
+++ ImageRecorder/scripts/ImageRecorder.lua
@@ -1,33 +1,9 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  ImageRecorder
-
-  Summary:
-  Recording images from a remote camera sensor to file.
-
-  Description:
-  This application connects to a remote camera sensor and records images which are
-  then stored on a private AppData folder in the device memory. The remote camera is
-  configured to have a frame rate of 5Hz.  The current image is viewed on the webpage.
-  This is useful to record a set of images in a real environment for playing later in
-  development environment.
-
-  How to run:
-  To run this sample a camera sensor has to be connected. The configuration of the camera
-  have to be adapted to meet the actual requirements.
-  The last 10 images are stored on the device. To retrieve from within AppStudio, the
-  location is found in "AppData" tab under "ImageRecorder" in the subfolder "LastRecord".
-  It must be noted, that the device memory should not be used for regular, cyclic write
-  operations, if possible mounted storage, e.g. an SD card should be used instead.
-
-  See also sample 'ImagePlayer'.
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Setting global values
 -- luacheck: globals gCAM_IP gRECORD_FOLDER gConfig gCam gViewer
-gCAM_IP = '192.168.0.100' -- Must be adapted to match the actual device
+gCAM_IP = '192.168.1.100' -- Must be adapted to match the actual device
 gRECORD_FOLDER = 'private/LastRecord' -- App private folder on device memory
 
 -- Camera Configuration 1
@@ -82,6 +58,9 @@ Script.register('Engine.OnStarted', main)
 -- img contains the image itself
 -- sensorData contains additional information about the image
 local function handleNewImage(img, sensorData)
+  -- clear view from earlier pictures
+  gViewer:clear()
+
   -- Create name for the output file
   local ts = SensorData.getTimestamp(sensorData)
   local fileName = gRECORD_FOLDER .. '/record_' .. ts .. '.bmp'
@@ -91,7 +70,8 @@ local function handleNewImage(img, sensorData)
   print("Recorded new Image file '" .. fileName .. "'")
 
   -- present the image in the image viewer
-  View.view(gViewer, img)
+  gViewer:addImage(img)
+  gViewer:present()
 end
 --Registration of the 'handleNewImage' function to the cameras 'OnNewImage' event
 Image.Provider.RemoteCamera.register(gCam, 'OnNewImage', handleNewImage)
--- readme.md
+++ readme.md
@@ -1,11 +1,13 @@
 ## ImageRecorder
 Recording images from a remote camera sensor to file.
+
 ### Description
 This application connects to a remote camera sensor and records images which are
 then stored on a private AppData folder in the device memory. The remote camera is
 configured to have a frame rate of 5Hz.  The current image is viewed on the webpage.
 This is useful to record a set of images in a real environment for playing later in
 development environment.
+
 ### How to Run
 To run this sample a camera sensor has to be connected. The configuration of the camera
 have to be adapted to meet the actual requirements.
